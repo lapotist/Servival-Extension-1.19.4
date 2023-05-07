27cd67edb90d6aa14c73e71c4f0530b17f28107f
lapotist <lapotist@gmail.com>
2023-05-07 20:17:11 +0800

    test commit

--- src/main/java/net/lapotist/survivalextension/SurvivalExtension.java
+++ src/main/java/net/lapotist/survivalextension/SurvivalExtension.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//test commit
 public class SurvivalExtension implements ModInitializer {
 	public static final String MOD_ID = "survival_extension";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
