5cfa31be1ef57a7dfaedf2dfac40b1b4382699a2
lapotist <lapotist@gmail.com>
2023-05-07 20:12:58 +0800

    Initial commit

---
+++ src/main/java/net/lapotist/survivalextension/SurvivalExtension.java
@@ -0,0 +1,15 @@
+package net.lapotist.survivalextension;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SurvivalExtension implements ModInitializer {
+	public static final String MOD_ID = "survival_extension";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lapotist/survivalextension/SurvivalExtensionClient.java
@@ -0,0 +1,11 @@
+package net.lapotist.survivalextension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SurvivalExtensionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
